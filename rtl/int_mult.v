df810dc3cf308aa8850456e3d971baae150e2205
kp-2002 <kaivalyaep22@gmail.com>
2024-09-26 20:07:46 +0000

    4th push

--- rtl/int_mult.v
+++ rtl/int_mult.v
@@ -6,6 +6,8 @@ module int_mult #(parameter DATA_WIDTH=32) (
 
 	parameter NUM_STAGES=$clog(DATA_WIDTH);
 
+	reg [DATA_WIDTH-1:0] result_intmd[DATA_WIDTH/2-1:0][NUM_STAGES-1:0];
+
 	genvar i,j;
 
 	generate
@@ -13,16 +15,16 @@ module int_mult #(parameter DATA_WIDTH=32) (
 			int_adder i_int_adder(
 				.data_a(m_cand & m_plier[j]),
 				.data_b(m_cand & m_plier[j+1]),
-				.result(result_intmd[j][i]));
+				.result(result_intmd[j][0]));
 		end
 	endgenerate
 
 	generate
 		for(i=1;i<NUM_STAGES;i=i+1) begin
-			for(j=0;j<(DATA_WIDTH/2**i);j=j+2) begin
+			for(j=0;j<(DATA_WIDTH/(2**i));j=j+2) begin
 				int_adder i_int_adder(
-					.data_a(m_cand & m_plier[j]),
-					.data_b(m_cand & m_plier[j+1]),
+					.data_a(result_intmd[j][i-1]),
+					.data_b(result_intmd[j+1][i-1]),
 					.result(result_intmd[j][i]));
 			end
 		end
