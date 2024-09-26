3fd45cecfcb45d4dc232dd174f6f1c2efc9ad969
kp-2002 <kaivalyaep22@gmail.com>
2024-09-26 19:58:45 +0000

    3rd push

--- rtl/int_mult.v
+++ rtl/int_mult.v
@@ -7,13 +7,24 @@ module int_mult #(parameter DATA_WIDTH=32) (
 	parameter NUM_STAGES=$clog(DATA_WIDTH);
 
 	genvar i,j;
-	generate
-		for(i=0;i<DATA_WIDTH/2;i=i+1) begin
-			int_adder i_int_adder(
-					.data_a(m_cand & m_plier[i]),
-					.data_b(m_cand & m_plier[i+1]),
-					.result(result_intmd[i][]));
 
+	generate
+		for(j=0;j<(DATA_WIDTH/2);j=j+2) begin
+			int_adder i_int_adder(
+				.data_a(m_cand & m_plier[j]),
+				.data_b(m_cand & m_plier[j+1]),
+				.result(result_intmd[j][i]));
+		end
+	endgenerate
+
+	generate
+		for(i=1;i<NUM_STAGES;i=i+1) begin
+			for(j=0;j<(DATA_WIDTH/2**i);j=j+2) begin
+				int_adder i_int_adder(
+					.data_a(m_cand & m_plier[j]),
+					.data_b(m_cand & m_plier[j+1]),
+					.result(result_intmd[j][i]));
+			end
 		end
 	endgenerate
 
