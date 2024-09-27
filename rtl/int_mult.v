fd05609231612127cb2f8281cf50d72b944ccf65
kp-2002 <kaivalyaep22@gmail.com>
2024-09-27 18:27:39 +0530

    Update int_mult.v
    Commit 6: Fixed some bugs in the implementation

--- rtl/int_mult.v
+++ rtl/int_mult.v
@@ -17,48 +17,86 @@ module int_mult #(parameter DATA_WIDTH=32)
 	parameter NUM_STAGES=$clog(DATA_WIDTH);
 	//number of stages in the multiplier
 
-	reg [DATA_WIDTH-1:0] result_intmd[DATA_WIDTH/2-1:0][NUM_STAGES-1:0];
+	reg [DATA_WIDTH-1:0] result_intmd[(DATA_WIDTH/2)-1:0][NUM_STAGES-1:0];
 
 	genvar i,j;
 
+	int_adder i_first_stage_left_int_adder(
+				.clk(clk),
+				.rst_n(rst_n),
+				.en(en),
+				.carry_in(),
+				.data_a(m_cand & m_plier[0]),
+				.data_b(m_cand & m_plier[1]),
+				.result(result_intmd[0][0]),
+				.carry_out());
+
 	generate
-		for(j=0;j<(DATA_WIDTH/2);j=j+2) begin
-			int_adder i_int_adder(
+		for(j=2;j<(DATA_WIDTH-3);j=j+2) begin
+			int_adder i_first_stage_int_adder(
 				.clk(clk),
 				.rst_n(rst_n),
 				.en(en),
 				.carry_in(),
 				.data_a(m_cand & m_plier[j]),
 				.data_b(m_cand & m_plier[j+1]),
-				.result(result_intmd[j][0])
+				.result(result_intmd[(j/2)][0]),
 				.carry_out());
 		end
 	endgenerate
 
+	int_adder i_first_stage_right_int_adder(
+			.clk(clk),
+			.rst_n(rst_n),
+			.en(en),
+			.carry_in(),
+			.data_a(m_cand & m_plier[(DATA_WIDTH-2)]),
+			.data_b(m_cand & m_plier[(DATA_WIDTH-1)]),
+			.result(result_intmd[((DATA_WIDTH/2)-1)][0]),
+			.carry_out());
+
 	generate
 		for(i=1;i<NUM_STAGES-1;i=i+1) begin
-			for(j=0;j<(DATA_WIDTH/(2**i));j=j+2) begin
-				int_adder i_int_adder(
+			int_adder i_intmd_stage_left_int_adder(
 					.clk(clk),
 					.rst_n(rst_n),
 					.en(en),
 					.carry_in(),
 					.data_a(result_intmd[j][i-1]),
 					.data_b(result_intmd[j+1][i-1]),
-					.result(result_intmd[j][i])
+					.result(result_intmd[j][i]),
+					.carry_out());
+			for(j=1;j<(DATA_WIDTH/(2**i)-1);j=j+2) begin
+				int_adder i_intmd_stage_int_adder(
+					.clk(clk),
+					.rst_n(rst_n),
+					.en(en),
+					.carry_in(),
+					.data_a(result_intmd[j][i-1]),
+					.data_b(result_intmd[j+1][i-1]),
+					.result(result_intmd[j][i]),
 					.carry_out());
 			end
+			int_adder i_intmd_stage_right_int_adder(
+					.clk(clk),
+					.rst_n(rst_n),
+					.en(en),
+					.carry_in(),
+					.data_a(result_intmd[j][i-1]),
+					.data_b(result_intmd[j+1][i-1]),
+					.result(result_intmd[j][i]),
+					.carry_out());
 		end
 	endgenerate
 
-	int_adder i_int_adder_final(
+	int_adder i_final_stage_int_adder(
 		.clk(clk),
 		.rst_n(rst_n),
 		.en(en),
 		.carry_in(),
 		.data_a(result_intmd[0][NUM_STAGES-2]),
 		.data_b(result_intmd[1][NUM_STAGES-2]),
-		.result(result[(3*DATA_WIDTH/2):(DATA_WIDTH/2)])
+		.result(result[(3*DATA_WIDTH/2):(DATA_WIDTH/2)]),
 		.carry_out());
 
 endmodule
